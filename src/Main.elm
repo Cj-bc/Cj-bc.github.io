16dba58c5758ad8e06006f8a4e3fc5956abb0041
Cj-bc <cj-bc@y-modify.org>
2019-06-14 12:57:58 +0900

    Change: move all elm codes into Main
    This is bacause:
      - It's recommended
      - I wanted to use 'utility' function in few files

--- src/Main.elm
+++ src/Main.elm
@@ -1,14 +1,11 @@
 port module Main exposing (main)
 
 import Browser
-import FavCharacter exposing (Character(..), Link(..))
 import Html exposing (Html, a, article, button, div, footer, form, header, img, input, label, li, nav, p, pre, text, textarea)
 import Html.Attributes exposing (class, for, href, id, name, src, title, type_)
 import Html.Events exposing (onClick)
 import List
-import Netlify exposing (netlify)
 import Octicons as Oct
-import Product exposing (Product(..))
 import VirtualDom
 
 
@@ -156,7 +153,7 @@ viewAboutme : Model -> Html Msg
 viewAboutme model =
     let
         (Character dic) =
-            FavCharacter.me
+            me
 
         base =
             [ img [ src dic.pic, title dic.name ] []
@@ -164,7 +161,7 @@ viewAboutme model =
             , div [ class "character-links" ] (List.map viewLink dic.links)
             , pre [ class "character-details" ] [ text dic.details ]
             , pre [ class "character-comments" ] [ text dic.comments ]
-            , viewFavs FavCharacter.characters
+            , viewFavs characters
             ]
     in
     case model.popupCh of
@@ -375,4 +372,154 @@ viewFooter =
 
 
 -- }}}
+-- }}}
+--
+--
+-- Netlify {{{
+
+
+netlify : String -> Html.Attribute msg
+netlify =
+    VirtualDom.attribute "netlify"
+
+
+
+-- }}}
+--
+-- Product {{{
+
+
+type alias Product =
+    { name : String
+    , description : String
+    }
+
+
+products : List Product
+products =
+    []
+
+
+
+-- }}}
+--
+-- Project {{{
+
+
+type alias Project =
+    { name : String
+    , comment : String
+    }
+
+
+
+-- }}}
+--
+-- FavCharacter {{{
+
+
+type alias Name =
+    String
+
+
+type alias Url =
+    String
+
+
+type alias Id =
+    String
+
+
+type Link
+    = Twitter Name
+    | Github Name
+    | Youtube Name Id
+    | Marshmallow Name
+    | Fanbox Name Id
+    | Other String Url
+
+
+type alias SrcPath =
+    String
+
+
+type Character
+    = Character
+        { name : String
+        , links : List Link
+        , details : String
+        , comments : String
+        , pic : SrcPath
+        }
+
+
+unlines : List String -> String
+unlines xs =
+    List.foldr (++) "" (List.intersperse "\n" xs)
+
+
+characters : List Character
+characters =
+    [ Character
+        { name = "魔王マグロナ"
+        , links =
+            [ Twitter "ukyo_rst"
+            , Youtube "まぐろなちゃんねる" "UCPf-EnX70UM7jqjKwhDmS8g"
+            , Marshmallow "ukyo_rst"
+            , Fanbox "ukyo_rst" "169083"
+            , Other "ukyoさんWiki" "https://dic.nicovideo.jp/a/ukyo%20rst"
+            ]
+        , details = "『バーチャルボイスチェンジお絵かきYoutuber魔王おじさんです。』"
+        , comments =
+            unlines
+                [ "とにかく超絶かわいいバ美肉元祖。"
+                , "バ美肉という概念をこの世に生み出してしまった元凶。彼女のおかげでどれだけの人の性癖が歪められたことか..."
+                , "数少ない「ボイスチェンジャー適合者」であり、初めてボイチェンをかけた時からかわいい女の子の声にしか聞こえないという神がかった声を持つおじさんである。"
+                , "本職は絵師(神絵師)だがVtuberとなったことで、ボイチェンの調整をし始め音屋さん並みに音響の話をする。"
+                , "魂のukyoさんは数年間もの生放送歴を持つベテランなのでめちゃくちゃ楽しい。普通に配信者としての面白さが半端ない。"
+                , "また、本職がクリエイターなのもあり、「コンテンツ」に対する考え方は尊敬に値する。というかそこだけじゃなく全てにおいてめっちゃ尊敬してますあのすごい"
+                ]
+        , pic = "https://yt3.ggpht.com/a/AGF-l7-nbjPwvmOOyrp7KlRKX9Xh5oiInNQFOSWCIg=s288-mo-c-c0xffffffff-rj-k-no"
+        }
+    , Character
+        { name = "兎鞠まり"
+        , links =
+            [ Twitter "tomari_mari"
+            , Youtube "Tomari Mari channel/兎鞠まりちゃんねる" "UCkPIfBOLoO0hVPG-tI2YeGg"
+            , Marshmallow "tomari_mari"
+            , Fanbox "兎鞠まり" "33648062"
+            , Other "Booth" "tomari-mari.booth.pm"
+            , Other "OpenREC" "https://www.openrec.tv/user/tomari_mari"
+            ]
+        , details = "『ボイチェンVTuberの兎鞠（とまり）まりです お菓子とげーむだいすき！』"
+        , comments =
+            unlines
+                [ "ロリ系バ美肉おじさんの筆頭。"
+                , "まぐろなちゃんの次くらいに出現した「ボイチェン適合者」。その可愛い声とあざとい仕草は人々を捉えて離さない。"
+                , "まぐろなちゃんの力でボイチェンが進化している「子供部屋」メンバーの一人。"
+                , "まぐろなちゃんと同じく本職は絵師。"
+                , "ukyoさん(まぐろなちゃんの魂)の配信の古参リスナーであり、ukyoさんをとても尊敬していた。絵師になったのもその影響のようである。かつて尊敬したukyoさんと今や仲良くコラボ配信等を行なっており、とても幸せそうでファンとしては本当に嬉しい。ちなみにまぐろなちゃんの(ukyoさんの)家と徒歩数分圏内に住んでいる。てぇてぇな。"
+                , "普段は所謂「メスガキ」ムーヴでイキリ散らかしているが、コンテンツへの考え方等しっかりしてるところはしっかりしており、とても安心できる。"
+                ]
+        , pic = "https://yt3.ggpht.com/a/AGF-l78KDm0T_7A0Kf9Fy9Z1nIgAYlyBr0IJ8sNprQ=s288-mo-c-c0xffffffff-rj-k-no"
+        }
+    ]
+
+
+me : Character
+me =
+    Character
+        { name = "Cj.bc_sd a.k.a Cj-bc"
+        , links =
+            [ Twitter "Cj_bc_sd"
+            , Github "Cj-bc"
+            , Other "Qiita" "https://qiita.com/Cj-bc"
+            ]
+        , details = " N odetails here"
+        , comments = "It's me! Obviously"
+        , pic = "assets/icon/cj-bc.jpg"
+        }
+
+
+
 -- }}}
--- src/Netlify.elm
+++
@@ -1,9 +0,0 @@
-module Netlify exposing (netlify)
-
-import Html
-import VirtualDom
-
-
-netlify : String -> Html.Attribute msg
-netlify =
-    VirtualDom.attribute "netlify"
